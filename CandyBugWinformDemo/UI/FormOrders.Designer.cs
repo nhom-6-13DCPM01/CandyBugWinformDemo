ce3b37d836874a2a3ccf8bb52fdefae508ded93f
quynhgiang26 <nguyenthiquynhgiang261200@gmail.com>
2021-10-22 19:36:55 +0700

    tạo chức năng đăng nhập

--- CandyBugWinformDemo/UI/FormOrders.Designer.cs
+++ CandyBugWinformDemo/UI/FormOrders.Designer.cs
@@ -371,7 +371,7 @@ namespace CandyBugWinformDemo.NewFolder1
             this.tabPage2.Location = new System.Drawing.Point(4, 25);
             this.tabPage2.Name = "tabPage2";
             this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(683, 484);
+            this.tabPage2.Size = new System.Drawing.Size(679, 484);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Candy";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -830,7 +830,7 @@ namespace CandyBugWinformDemo.NewFolder1
             this.tabControl1.Location = new System.Drawing.Point(0, 68);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(691, 513);
+            this.tabControl1.Size = new System.Drawing.Size(687, 513);
             this.tabControl1.TabIndex = 0;
             // 
             // tabPage1
